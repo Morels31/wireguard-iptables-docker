67f2c11c4a8f2b9b1e98b4218e629d3fb8552081
Morels31 <clod.is.here@gmail.com>
2024-04-26 01:22:34 +0200

    Restructured startup script, added optional custom script

--- startup.sh
+++ startup.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
 #Specify log folder location
-logFolder="/logs/"
+logDir="/logs"
 
+#Where the script expects needed files
+configDir="/config"
 
 
 logg(){
 	toLog="$(printf "%s - %s\n" "$(date +'%d/%m/%Y %T')" "$1")"
-	echo "$toLog" | tee -a "$logFolder"startup-log
+	echo "$toLog" | tee -a $logDir/startup-log
 }
 
 errorr(){
@@ -18,29 +20,36 @@ errorr(){
 
 
 
-mkdir -p "$logFolder"
+mkdir -p $logDir
 logg "Startup script started"
 
 
 
 #Check needed files existence
-test -f /etc/iptables/rules.v4 || errorr "/etc/iptables/rules.v4 file not found"
+test -r $configDir/rules.v4 || errorr "rules.v4 file not found"
 
-if [ ! -f /etc/iptables/rules.v6 ]; then
-	logg "/etc/iptables/rules.v6 file not found, using default (DROP every ipv6 packet)"
-	printf "*filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT DROP [0:0]\nCOMMIT\n" > /etc/iptables/rules.v6
+if [ ! -r $configDir/rules.v6 ]; then
+	logg "rules.v6 file not found, using default (DROP every ipv6 packet)"
+	printf "*filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT DROP [0:0]\nCOMMIT\n" > $configDir/rules.v6
 fi
 
-test -f /etc/wireguard/wg0.conf || errorr "/etc/wireguard/wg0.conf file not found"
+test -r $configDir/wg0.conf || errorr "wg0.conf file not found"
+cp $configDir/wg0.conf /etc/wireguard/wg0.conf || errorr "Failed copying wg0.conf file"
 
 
 
 #Setup iptables
-iptables-restore -n /etc/iptables/rules.v4 || errorr "Failed restoring ipv4 iptables"
-ip6tables-restore -n /etc/iptables/rules.v6 || errorr "Failed restoring ipv6 iptables"
+iptables-restore -n $configDir/rules.v4 || errorr "Failed restoring ipv4 iptables"
+ip6tables-restore -n $configDir/rules.v6 || errorr "Failed restoring ipv6 iptables"
 
 #Setup wireguard
 wg-quick up wg0 || errorr "Failed starting wireguard client"
+
+if [ -x $configDir/custom-script.sh ]; then
+	logg "Executing custom-script.sh"
+	$configDir/custom-script.sh || errorr "custom-script.sh failed"
+	logg "custom-script.sh completed successfully"
+fi
 
 
 
